99e7e83ddf15ad0495eb0a3034ea3aa79876ef66
AbdulHannan-123 <ah6475774@gmail.com>
2023-02-26 01:31:19 +0500

    half properties of flutter text widget

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TextWidgetDemo(),
+    );
+  }
+}
+
+class TextWidgetDemo extends StatelessWidget {
+  const TextWidgetDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: 150,
+          child: Text(
+            "My Name is Abdul Hannan My Name is Abdul Hannan My Name is Abdul Hannan My Name is Abdul Hannan My Name is Abdul HannanMy Name is Abdul Hannan",
+            key: key,  // it identify the element tree of the current state of widget
+
+            // strutStyle:const StrutStyle(   // almost same as the TextStyle of the text
+
+            //   // leadingDistribution: TextLeadingDistribution(),  //Does not affect layout when TextStyle.height is not specified
+            //   // leading: 3,   // increase the space between the lines
+            //   fontFamily: 'Roboto',
+            //   // fontSize: 30,
+            //   height: 1.5, // this also give the height to the text
+            //   // forceStrutHeight: false    // This property guarantees uniform line spacing, 
+            // ),
+
+            // locale: ,  //Used to select a font when the same Unicode character can be rendered differently
+
+            // maxLines: 3,   // how many lines to show  maximum  if its null then you can just have a vast ammount of line dynamically
+
+            // overflow: TextOverflow.visible,  // this will force to overflow the widget
+            //overflow: TextOverflow.ellipsis    // this will leave ... if the string is not fixing in the perticular area
+            //overflow: TextOverflow.clip    // this will just crop the perticular over flow
+            // overflow: TextOverflow.fade     // is also crop the perticular string with fade effects
+
+            // selectionColor: Colors.red,     // The color to use when painting the selection.
+
+            // semanticsLabel:  'Double dollars', //This is useful for replacing abbreviations or shorthands with the full text value:
+
+            
+  
+          ),
+        ),
+      ),
+    );
+  }
+}
